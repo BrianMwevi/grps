19d0e9887c10bcf1e2e506f8e8c373d0ed3db52d
brianmwevi <mwevibrian@gmail.com>
2020-03-16 02:19:49 +0300

    Reduced 4 lines of code

--- grps.py
+++ grps.py
@@ -1,18 +1,15 @@
 import random as rand
+import string as alf
 m_names = input("Enter file name and omit '.txt': ").lower()
 per_grp = int(input("\nEnter people per group: "))
 members = open(m_names + ".txt", "r").readlines() # Open the inserted file
 all_plys = len(members)
-counter = 0 # Counts and increment number of groups
-results = {}
+grp_count = 0 # Counts and increment number of groups
 rand.shuffle(members)
 
 for i in range(0,all_plys,per_grp): # Steps with the number of people/group
-	counter += 1
+	grp_count += 1
 	team = members[i:i+per_grp]
-	results["Group " + str(counter)] = team
-for grp in range(len(results)):
-	print("{} {}:".format("Group",grp + 1))
-	vals = results["Group " + str(grp+1)]
-	for v in vals:
-		print("\t",v, end="")
+	print("Group {}:".format(alf.ascii_uppercase[grp_count-1]))
+	for i in range(len(team)):
+		print ("\t {}. {}".format(i+1,team[i]))
